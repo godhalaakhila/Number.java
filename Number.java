7aa50a5e065428426850c5f5f15719e3c474a5bd
godhalaakhila <akhilareddyg55@gmail.com>
2025-01-30 17:45:16 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,19 @@
+public class Number {
+
+    public static void main(String[] args) {
+        // Define a number (you can change this number to test different values)
+        int number = 5;
+
+        // Check if the number is positive, negative, or zero
+        if (number > 0) {
+            System.out.println("The number " + number + " is positive.");
+        } else if (number < 0) {
+            System.out.println("The number " + number + " is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
+ 
+    
+
